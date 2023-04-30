51315ffa585b59ada94ec1559dffffe7c7a27b80
sabonrex <118281670+sabonrex@users.noreply.github.com>
2023-04-30 16:06:09 +0200

    okay

--- src/js/views/home.jsx
+++ src/js/views/home.jsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import { Context } from "../store/appContext";
 
 export const Home = () => (
-	
+
+    const { store, action } = useContext(Context);
+
+    useEffect  ( ()=> {
+
+    },[])
+
+    return(
+    <div class="jumbotron">
+    <h1 class="display-4">Hello, world!</h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4"/>
+                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+             </p>
+</div>
+    )
+    
 );
