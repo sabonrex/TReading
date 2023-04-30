511dc178703d3dddbfd233ce82a3df16feb31698
SabonRex <sabona.a.rexwinkel@gmail.com>
2023-04-30 04:41:00 +0200

    update

--- src/js/component/navbar.jsx
+++ src/js/component/navbar.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Container, { Nav } from "react-bootstrap";
+import Col from "react-bootstrap";
+import Row from "react-bootstrap";
+import { Navbar} from "react-bootstrap"
+import Image from 'react-bootstrap/Image'
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
-export const Navbar = () => {
+export const NavbarMenu = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+		<Navbar>
+			<Navbar.Brand>
+				<Link to = "/">
+					<Image
+						src="src/img/Star_Wars_Logo..png"
+						height="40"
+						alt="Star Wars"
+					/>
 				</Link>
-			</div>
-		</nav>
+			</Navbar.Brand>
+			<Nav className='mr-auto'>
+			<Link className="nav-link" to = "/planet">Planets</Link>
+			<Link className="nav-link" to = "/people">People</Link>
+			<Link className="nav-link" to = "/vehicle">Vehicles</Link>
+
+			</Nav>
+				<DropdownButton id="dropdown-basic-button" title="Favourites">
+      				<Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+      				<Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+      				<Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+    			</DropdownButton>
+		</Navbar>
 	);
 };
--- src/js/layout.jsx
+++ src/js/layout.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
-
+import Container, { Navbar } from "react-bootstrap";
+import Col from "react-bootstrap";
+import Row from "react-bootstrap";
+import { Navbar } from "react-bootstrap";
 import { Home } from "./views/home";
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
-
-import { Navbar } from "./component/navbar";
-import { Footer } from "./component/footer";
+import People from "./views/people";
+import Planets from "./views/planet";
+import Vehicles from "./views/vehicle";
+import { NavbarMenu } from "./component/navbar";
 
 //create your first component
 const Layout = () => {
@@ -17,20 +21,30 @@ const Layout = () => {
 	const basename = process.env.BASENAME || "";
 
 	return (
-		<div>
+		<Container>
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
+				<Routes> 
+					<Row>
+						<Col>
+						<NavbarMenu/>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+						<Route element={<StarWars />} path="/" />
+                        <Route element={<Demo />} path="/demo" />
+                        <Route element={<People />} path="/people/:uid" />
+                        <Route element={<Planets />} path="/planets/:uid" />
+                        <Route element={<Vehicles />} path="/vehicles/:uid" />
+                        <Route element={<h1>Not found! 404</h1>} path="*" />
+						</Col>
+					</Row>
+                    </Routes>
+
 				</ScrollToTop>
 			</BrowserRouter>
-		</div>
+		</Container>
 	);
 };
 
