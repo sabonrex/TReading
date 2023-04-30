0cbfa1a18b474082ad5cd62907a51128efb1cf05
sabonrex <sabona.a.rexwinkel@gmail.com>
2023-04-30 21:54:59 +0000

    update

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,11 +1,8 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
-			planet: [{name: "info"}],
-			people: [{name: "info"}],
-      		singlefetch: [],
-	  		singleview: [],	
-			vehicle: [{name: "info"}],
+			planetsList: [],
+			peopleList: [],
 			favorites: []
 		},
 		actions: {
@@ -20,7 +17,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const response = await fetch(URL, CONFIG);
 				const json = await response.json();
 				console.log(">>DATA>>", json);
-				setStore({ people: json.results });
+				setStore({ peopleList: json.results });
 			},
 			fetchPlanets: async () => {
 				const URL = "https://swapi.dev/api/planets/";
@@ -33,26 +30,8 @@ const getState = ({ getStore, getActions, setStore }) => {
 				const response = await fetch(URL, CONFIG);
 				const json = await response.json();
 				console.log(">>DATAPlanets>>", json);
-				setStore({ planet: json.results });
+				setStore({ planetsList: json.results });
 			},
-      		fetchVehicle: async () => {
-				const URL = "https://swapi.dev/api/vehicle/";
-				const CONFIG = {
-					method: "GET",
-					headers: {
-						"Content-type": "application/json"
-					}
-				};
-				const response = await fetch(URL, CONFIG);
-				const json = await response.json();
-				console.log(">>DATA>>", json);
-				setStore({ vehicle: json.results });
-			},
-			loadSomeData: (key, passedData) => {
-				setStore({"singleview" : {name : "info"}}),
-				setStore({[key] : passedData})
-			},
-
 			setFavorites: name => {
 				const store = getStore();
 				setStore({ favorites: [...store.favorites, name] });
